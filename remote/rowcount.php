bb0bdae8a1b77ff7e5a433362e1e02a36edccadd
Shaileshprajapati-dev <95132308+Shaileshprajapati-dev@users.noreply.github.com>
2022-02-13 09:41:16 +0530

    Add files via upload

---
+++ remote/rowcount.php
@@ -0,0 +1,39 @@
+<?php
+
+#step1 : make connection
+
+include __DIR__.'/dbconnect.php';
+require __DIR__,'/vendor/autoload.php';
+
+$table=new LucidFrame\Console\ConsoleTable();
+$table
+->addHeader('Sr no.')
+->addHeader('id')
+->addHeader('name')
+->addHeader('email');
+
+$sql= "SELECT * FROM emp";
+
+$result_set = mysqli_query($count,$sql);
+
+if(mysqli_num_rows($result_set)>0){
+	
+	$i=1;
+	while($row=mysqli_fetch_assoc($result_set)){
+		 
+		 $table->addrow()
+		 ->addColumn($i)
+		 ->addColumn($row['id'])
+		 ->addColumn($row['name'])
+		 ->addColumn($row['email']);
+		 $i++;
+	}
+	$table->display();
+}else if(mysqli_num_rows($result_set)==0){
+	echo 'No recound found';
+	
+}else{
+	 echo 'Inserted Error'.mysqli_error($conn);
+}
+
+?>
---
+++ remote/setting.php
@@ -0,0 +1,24 @@
+<?php
+
+//This is Setting file
+
+################ All Constants ##########################
+define('HOST','145.14.153.101');
+define('USER','u323470395_vsfIs');
+define('PASSWORD','5~+Fs@M1oK');
+define('DBNAME','u323470395_3VqkN');
+define('DB_CHECK',false);
+
+################ All Constants ##########################
+
+return [
+	
+	'db:config'=>[
+		
+		'host'=>'145.14.153.101',
+		'user'=>'u323470395_vsfIs',
+		'password'=>'5~+Fs@M1oK',
+		'dbname'=>'u323470395_3VqkN',
+	]
+
+];
